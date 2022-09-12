4a5b474b6935b1e819b8f1a15ad4512599129b9e
unknown <you@example.com>
2022-09-12 10:08:55 +0200

    Creation of the user_entity with constructors,getters and setters

---
+++ src/main/java/com/appli/chatflow/entity/Utilisateur.java
@@ -0,0 +1,59 @@
+package com.appli.chatflow.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "utilisateur")
+public class Utilisateur {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_utilisateur")
+    @SequenceGenerator(name = "seq_utlisateur", allocationSize = 1)
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	
+	public Utilisateur() {
+		super();
+	}
+
+	public Utilisateur(int id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+
+}
