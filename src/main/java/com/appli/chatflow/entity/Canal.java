3582af97e014e4df15d4d7891006cd6de9a45d0f
iqra tariq <tariqn.iqra@gmail.com>
2022-09-12 14:55:35 +0200

    Creation of the entity Canal and MVC , applied Crud on it

---
+++ src/main/java/com/appli/chatflow/entity/Canal.java
@@ -0,0 +1,58 @@
+package com.appli.chatflow.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity 
+@Table( name = "canal")
+public class Canal {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_canal")
+    @SequenceGenerator(name = "seq_canal", allocationSize = 1)
+	
+	private int id ; 
+	@Column(nullable = false , unique= true)
+	private String nom ;
+	
+	
+	public Canal() {
+		super();
+	}
+
+
+	public Canal(int id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	} 
+	
+	
+
+	
+}
---
+++ src/main/java/com/appli/chatflow/repository/CanalRepository.java
@@ -0,0 +1,10 @@
+package com.appli.chatflow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.appli.chatflow.entity.Canal;
+
+
+public interface CanalRepository extends JpaRepository<Canal, Integer>{
+
+}
