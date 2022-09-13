c6ef4d06628854f01a386380d6822eb13ffdf9d6
iqra tariq <tariqn.iqra@gmail.com>
2022-09-13 09:56:33 +0200

    modification in service and add comments in it

--- src/main/java/com/appli/chatflow/entity/Canal.java
+++ src/main/java/com/appli/chatflow/entity/Canal.java
@@ -10,29 +10,25 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-@Entity 
-@Table( name = "canal")
+@Entity
+@Table(name = "canal")
 public class Canal {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_canal")
-    @SequenceGenerator(name = "seq_canal", allocationSize = 1)
-	
-	private int id ; 
-	@Column(nullable = false , unique= true)
-	private String nom ;
-	
+	@SequenceGenerator(name = "seq_canal", allocationSize = 1)
+
+	private int id;
+	@Column(nullable = false, unique = true)
+	private String nom;
+
 	@ManyToOne
-	@JoinColumn(name ="id_utilisateur")
-	private Utilisateur utilisateur; 
-	
-	
-	
-	
+	@JoinColumn(name = "id_utilisateur")
+	private Utilisateur utilisateur;
+
 	public Canal() {
 		super();
 	}
-
 
 	public Canal(int id, String nom) {
 		super();
@@ -40,38 +36,28 @@ public class Canal {
 		this.nom = nom;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getNom() {
 		return nom;
 	}
-
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-
 	public Utilisateur getUtilisateur() {
 		return utilisateur;
 	}
 
-
 	public void setUtilisateur(Utilisateur utilisateur) {
 		this.utilisateur = utilisateur;
-	} 
-	
-	
-	
+	}
 
-	
 }
