a27d6f70919acb3f1cc2316dc477ede44fbd7d07
iqra tariq <tariqn.iqra@gmail.com>
2022-09-29 18:54:55 +0200

    adaptation of back end

--- src/main/java/com/appli/chatflow/ChatFlowApplication.java
+++ src/main/java/com/appli/chatflow/ChatFlowApplication.java
@@ -2,9 +2,10 @@ package com.appli.chatflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,5 +14,14 @@ public class ChatFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatFlowApplication.class, args);
+	}
+	
+	@EnableWebMvc
+	public class CorsConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	registry.addMapping("/**").allowedOrigins("http://localhost:4200/")
+	.allowedMethods("GET", "POST", "PUT","DELETE");
+	}
 	}
 }
